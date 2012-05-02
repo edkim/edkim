a577556ff0788294ebdd348812c2cac46a70b1a6
Edward Kim <edk217@gmail.com>
2012-05-01 23:37:33 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Edkim::Application.config.secret_token = '2057feda0d62c17e41fb0743d7cd1d85424c0786db8c022458109473ef4d8daededd68fb620e30c0d7fc69f5ac002a1684807062152c491705bfed5674f9004a'
